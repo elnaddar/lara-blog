ea66bbaad59998c18b6684c821090062540ba7a9
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-05-23 13:49:03 +0300

    used named routes to navigate in the app.

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -49,7 +49,7 @@
                         <td>{{ $post['posted_by'] }}</td>
                         <td>{{ $post['created_at'] }}</td>
                         <td>
-                            <a href="/posts/{{ $post['id'] }}" class="btn btn-info">View</a>
+                            <a href="{{ route('posts.show', $post['id']) }}" class="btn btn-info">View</a>
                             <a href="#" class="btn btn-primary">Edit</a>
                             <a href="#" class="btn btn-danger">Delete</a>
                         </td>
